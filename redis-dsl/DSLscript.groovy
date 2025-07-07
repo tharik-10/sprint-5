9e141375fb8f74108a6471a661fa1daea2af7fb3
tharik-10 <md.tharik@mygurukulam.co>
2025-07-07 19:48:25 +0530

    Create DSLscript.groovy

---
+++ redis-dsl/DSLscript.groovy
@@ -0,0 +1,24 @@
+// Create the complete folder hierarchy step by step
+folder('Terraform Infrastructure Jobs/env') {}
+folder('Terraform Infrastructure Jobs/env/dev') {}
+folder('Terraform Infrastructure Jobs/env/dev/wrappercode') {}
+folder('Terraform Infrastructure Jobs/env/dev/wrappercode/databse') {} 
+folder('Terraform Infrastructure Jobs/env/dev/wrappercode/database/redis') {}
+
+pipelineJob('Terraform Infrastructure Jobs/env/dev/wrappercode/database/redis/dev-otms-seed-job-wrappercode-redis') {
+    displayName('dev-otms-seed-job-wrappercode-redis')
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/Cloud-NInja-snaatak/jenkins-pipeline.git')
+                        credentials('github-token')
+                    }
+                    branches('*/Aniruddh_SCRUM-526')
+                }
+            }
+            scriptPath('terraform/wrappercode/env/dev/database/redis/Jenkinsfile')
+        }
+    }
+}
