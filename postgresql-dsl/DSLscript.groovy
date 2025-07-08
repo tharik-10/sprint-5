0075db8e02fb4d9eb49e8df5c6315c6d239a898b
tharik-10 <md.tharik@mygurukulam.co>
2025-07-08 12:25:12 +0530

    Update DSLscript.groovy

--- postgresql-dsl/DSLscript.groovy
+++ postgresql-dsl/DSLscript.groovy
@@ -1,11 +1,11 @@
 // Create the complete folder hierarchy step by step
 folder('Terraform Infrastructure Jobs/env') {}
-folder('Terraform Infrastructure Jobs/env/dev') {}
-folder('Terraform Infrastructure Jobs/env/dev/wrappercode') {}
-folder('Terraform Infrastructure Jobs/env/dev/wrappercode/database') {} 
-folder('Terraform Infrastructure Jobs/env/dev/wrappercode/database/postgresql') {}
+folder('Terraform Infrastructure Jobs/env/qa') {}
+folder('Terraform Infrastructure Jobs/env/qa/wrappercode') {}
+folder('Terraform Infrastructure Jobs/env/qa/wrappercode/database') {} 
+folder('Terraform Infrastructure Jobs/env/qa/wrappercode/database/postgresql') {}
 
-pipelineJob('Terraform Infrastructure Jobs/env/dev/wrappercode/database/postgresql/dev-otms-seed-job-wrappercode-postgresql') {
+pipelineJob('Terraform Infrastructure Jobs/env/qa/wrappercode/database/postgresql/dev-otms-seed-job-wrappercode-postgresql') {
     displayName('dev-otms-seed-job-wrappercode-postgresql')
     definition {
         cpsScm {
@@ -18,7 +18,7 @@ pipelineJob('Terraform Infrastructure Jobs/env/dev/wrappercode/database/postgres
                     branches('*/Tharik_SCRUM-536')
                 }
             }
-            scriptPath('terraform/wrappercode/env/dev/database/postgresql/Jenkinsfile')
+            scriptPath('terraform/wrappercode/env/qa/database/postgresql/Jenkinsfile')
         }
     }
 }
